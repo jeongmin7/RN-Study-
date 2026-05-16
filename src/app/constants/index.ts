e6db6287e00d54f35c69e5973b197a79f16b50fe
jeongmin <lee.jeongmin1625@gmail.com>
2026-05-16 22:33:59 +0900

    chore: add design system color token

---
+++ src/app/constants/index.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  Gray900: "#191D30",
+  Gray300: "#8C8E97",
+  Gray200: "#E1E1E1",
+  Gray100: "#F1F1F1",
+  Neutral100: "#F2F6F7",
+  Blue100: "#ECF8FF",
+  Blue500: "#1892FA",
+};
