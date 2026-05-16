7335bc23a9802dc88fb2c574a253502aa6c299a6
jeongmin <lee.jeongmin1625@gmail.com>
2026-05-16 22:58:42 +0900

    feat: add MedicationItem component and render on home tab

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,11 @@
-import { router } from "expo-router";
-import { Button, Pressable, StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MedicationItem from "../components/MedicationItem";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        title="버튼"
-        onPress={() => {
-          console.log("클릭됨!");
-        }}
-      ></Button>
-      <Pressable onPress={() => router.push("/create")}>
-        <Text style={styles.text}>일정 스크린으로 이동</Text>
-      </Pressable>
+      <MedicationItem name="비타민" memo="1정" timing="식후 30분 " />
     </SafeAreaView>
   );
 }
@@ -21,10 +13,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "#009be6",
+    padding: 16,
     backgroundColor: "#fff",
-  },
-  text: {
-    color: "#009be6",
   },
 });
---
+++ src/app/components/MedicationItem.tsx
@@ -0,0 +1,58 @@
+import React, { ReactNode } from "react";
+import { StyleSheet, View } from "react-native";
+import { colors } from "../constants";
+import Badge from "./Badge";
+import Typography from "./Typography";
+
+interface MedicationItemProps {
+  header?: ReactNode;
+  footer?: ReactNode;
+  name: string;
+  memo: string;
+  timing?: string;
+}
+
+function MedicationItem({
+  header,
+  footer,
+  name,
+  memo,
+  timing,
+}: MedicationItemProps) {
+  return (
+    <View style={styles.container}>
+      {header}
+      <View style={styles.contentsContainer}>
+        <View style={{ flex: 1, gap: 3 }}>
+          <Typography variant="lg">{name}</Typography>
+          <View style={styles.descriptionContainer}>
+            <Typography variant="sm">{memo} </Typography>
+            {timing && <Badge name={timing} />}
+          </View>
+        </View>
+      </View>
+      {footer}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.Gray100,
+    borderRadius: 24,
+    paddingVertical: 20,
+    paddingHorizontal: 24,
+    backgroundColor: "#fff",
+  },
+  contentsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  descriptionContainer: {
+    flexDirection: "row",
+  },
+});
+
+export default MedicationItem;
