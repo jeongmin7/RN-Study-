a53e25ba2002a37216d0bad0f9b67a985d672bd9
jeongmin <lee.jeongmin1625@gmail.com>
2026-05-16 22:37:20 +0900

    feat: apply SafeAreaView and title to schedules tab

--- src/app/(tabs)/schedules.tsx
+++ src/app/(tabs)/schedules.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Typography from "../components/Typography";
 
 export default function Schedules() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>일정</Text>
+      <Typography variant="xxl">일정</Typography>
     </SafeAreaView>
   );
 }
@@ -13,10 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#009be6",
   },
 });
