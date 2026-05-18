bc8d1a1021b1b9f87fd0238fb42051a86a434afb
jeongmin <lee.jeongmin1625@gmail.com>
2026-05-18 23:18:19 +0900

    feat: create database

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,67 @@
+import { useSQLiteContext } from "expo-sqlite";
+import { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from "../components/CustomButton";
-import MedicationItem from "../components/MedicationItem";
 
+const DATA = [
+  {
+    title: "08:00",
+    data: [
+      { id: "1", name: "비타민", memo: "1정", timing: "식후 30분" },
+      { id: "2", name: "오메가3", memo: "2정", timing: "식후 30분" },
+    ],
+  },
+  {
+    title: "12:00",
+    data: [
+      { id: "1", name: "칼슘", memo: "1정", timing: "식후 30분" },
+      { id: "2", name: "마그네슘", memo: "1정", timing: "식후 30분" },
+    ],
+  },
+  {
+    title: "18:00",
+    data: [
+      { id: "1", name: "비타민D", memo: "1정", timing: "식후 30분" },
+      { id: "2", name: "프로바이오틱스", memo: "1정", timing: "식후 30분" },
+    ],
+  },
+];
 export default function Home() {
+  const db = useSQLiteContext();
+  useEffect(() => {
+    (async () => {
+      await db.runAsync(
+        `INSERT INTO medications (name, color) VALUES (?, ?)`,
+        "비타민",
+        "blue",
+      );
+    })();
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <MedicationItem
+    <SafeAreaView edges={["top"]} style={styles.container}>
+      {/* <MedicationItem
         name="비타민"
         memo="1정"
         timing="식후 30분"
         footer={<CustomButton label="추가" size="medium" />}
-      />
+      /> */}
+      {/* <SectionList
+        sections={DATA}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <MedicationItem
+            name={item.name}
+            memo={item.memo}
+            timing={item.timing}
+            footer={<CustomButton label="추가" size="medium" />}
+          />
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Typography variant="lg">{title}</Typography>
+        )}
+        contentContainerStyle={{ padding: 16, gap: 10 }}
+      /> */}
     </SafeAreaView>
   );
 }
@@ -19,7 +69,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    // padding: 16,
     backgroundColor: "#fff",
   },
 });
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,59 @@
 import { Stack } from "expo-router";
+import * as SQLite from "expo-sqlite";
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
+import { Suspense } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+async function migrateDbIfNeeded(db: SQLiteDatabase) {
+  const DATABASE_VERSION = 1;
+  let { user_version: currentDbVersion } = (await db.getFirstAsync<{
+    user_version: number;
+  }>("PRAGMA user_version")) ?? { user_version: 0 };
+  if (currentDbVersion >= DATABASE_VERSION) {
+    return;
+  }
+  if (currentDbVersion === 0) {
+    // `execAsync()` is useful for bulk queries when you want to execute altogether.
+    // Note that `execAsync()` does not escape parameters and may lead to SQL injection.
+    await db.execAsync(`
+PRAGMA journal_mode = WAL;
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE if not exists medications (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  color TEXT NOT NULL
+);
+`);
+    currentDbVersion = 1;
+  }
+
+  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
+}
+
+const db = SQLite.openDatabaseAsync("test.db2");
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="create" options={{ headerShown: false }} />
-    </Stack>
+    <Suspense
+      fallback={
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      }
+    >
+      <SQLiteProvider
+        databaseName="test.db2"
+        onInit={migrateDbIfNeeded}
+        useSuspense
+      >
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="create" options={{ headerShown: false }} />
+        </Stack>
+      </SQLiteProvider>
+    </Suspense>
   );
 }
